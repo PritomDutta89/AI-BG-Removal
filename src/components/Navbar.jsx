4035d0e84ad5b40a2ab8e636b6279d682742299b
pritom.dutta <pritom.dutta@rahitechnologies.com>
2025-05-27 22:03:29 +0530

    Integrated clerk for authentication

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const { openSignIn } = useClerk();
+  const { isSignedIn, user } = useUser(); //get user data from clerk
 
   return (
     <div className="flex justify-between items-center mx-[1rem] md:mx-[4rem] lg:mx-[7rem] py-[1rem]">
@@ -13,17 +16,24 @@ const Navbar = () => {
         className="w-[7rem] md:w-[10rem] lg:w-[12rem] cursor-pointer"
         onClick={() => navigate("/")}
       />
-      <button
-        type="button"
-        className="text-white flex justify-center items-center gap-3 bg-[#313131] hover:bg-gray-900 focus:outline-none font-medium rounded-full text-[0.6rem] md:text-sm px-3 md:px-5 py-1.5 md:py-2.5"
-      >
-        Get started
-        <img
-          src={assets.arrow_icon}
-          alt="icon"
-          className="w-[0.5rem] md:w-[0.8rem]"
-        />
-      </button>
+      {isSignedIn ? (
+        <div>
+          <UserButton />
+        </div>
+      ) : (
+        <button
+          type="button"
+          className="text-white flex justify-center items-center gap-3 bg-[#313131] hover:bg-gray-900 focus:outline-none font-medium rounded-full text-[0.6rem] md:text-sm px-3 md:px-5 py-1.5 md:py-2.5"
+          onClick={() => openSignIn({})}
+        >
+          Get started
+          <img
+            src={assets.arrow_icon}
+            alt="icon"
+            className="w-[0.5rem] md:w-[0.8rem]"
+          />
+        </button>
+      )}
     </div>
   );
 };
